dfb120508aa58b160b905c75012ab0bb62c73e56
Maneesh Wije <m.mwije1@proton.me>
2024-12-13 18:49:15 -0500

    day13 p2

--- aoc/2024/day13/main.py
+++ aoc/2024/day13/main.py
@@ -34,5 +34,34 @@ def part1():
 
     print(res)
 
+def part2():
+    with open("real_input", "r") as file:
+        data = file.read().strip()
+        section = data.split("\n\n")
+    res = 0
+    for s in section:
+        button_a = [
+            int(x)
+            for match in re.findall(r"Button A: X\+(\d+), Y\+(\d+)", s)
+            for x in match
+        ]
+        button_b = [
+            int(x)
+            for match in re.findall(r"Button B: X\+(\d+), Y\+(\d+)", s)
+            for x in match
+        ]
+        prize = [
+            int(x) for match in re.findall(r"Prize: X=(\d+), Y=(\d+)", s) for x in match
+        ]
+        prize[0] = prize[0] + 10000000000000
+        prize[1] = prize[1] + 10000000000000
+        press_a = (prize[0] * button_b[1] - prize[1] * button_b[0]) / (button_a[0] * button_b[1] - button_a[1] * button_b[0])
+        press_b = (prize[0] - button_a[0] * press_a) / button_b[0]
+        # if whole number, its valid
+        if press_a % 1 == 0 and press_b % 1 == 0:
+            res += int(press_b + (3 * press_a))
 
-part1()
+    print(res)
+
+# part1()
+part2()
